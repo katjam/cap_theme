9b3dfbd3098cafc33b0084f36ab7718385a03043
katjam <katja@neontribe.co.uk>
2016-07-10 17:59:56 +0100

    Change link to chicklit

--- page-home.php
+++ page-home.php
@@ -1,24 +1,24 @@
 <?php get_header(); ?>
 
-			<div id="content">
+            <div id="content">
 
-				<div id="inner-content" class="wrap cf">
+                <div id="inner-content" class="wrap cf">
 
-						<main id="main" role="main" itemprop="mainContentOfPage" >
+                        <main id="main" role="main" itemprop="mainContentOfPage" >
               <div class="section clearfix">
-                <h2>Cinema</h2><div><h3><a href="/screenings">CURRENTLY SHOWING - Draw on Sweet Night</a></h3></div>
+                <h2>Cinema</h2><div><h3><a href="/screenings">COMING SOON - ChickLit</a></h3></div>
                 <?php $args = array( 'post_type' => 'capriol-film', 'production-types' => 'cinema' ); ?>
                 <?php $loop = new WP_Query( $args ); ?>
                 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-							  <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-                    <section class="entry-content" > 
-                      <a href ="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('cap-thumb'); ?></a>              
-									    <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                    </section>  
-							  </article>
-           
-							<?php endwhile; wp_reset_query(); ?>
+                              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+                    <section class="entry-content" >
+                      <a href ="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('cap-thumb'); ?></a>
+                                        <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                    </section>
+                              </article>
+
+                            <?php endwhile; wp_reset_query(); ?>
               </div>
               <div class="section clearfix">
                 <h2>Television</h2>
@@ -26,20 +26,20 @@
                 <?php $loop = new WP_Query( $args ); ?>
                 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-							  <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-                  <section class="entry-content" > 
-                    <a href ="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('cap-thumb'); ?></a>              
-									  <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                  </section>  
-							  </article>
-             
-						 	<?php endwhile; wp_reset_query(); ?>
+                              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+                  <section class="entry-content" >
+                    <a href ="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('cap-thumb'); ?></a>
+                                      <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                  </section>
+                              </article>
+
+                            <?php endwhile; wp_reset_query(); ?>
               </div>
-						</main>
+                        </main>
 
-				</div>
+                </div>
 
-			</div>
+            </div>
 
 
 <?php get_footer(); ?>

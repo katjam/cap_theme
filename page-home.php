413b556ac43c2f44224aa1641f5c260fb80f7f8c
katjam <katja@neontribe.co.uk>
2017-10-20 19:57:37 +0100

    Removed coming soon

--- page-home.php
+++ page-home.php
@@ -6,12 +6,12 @@
 
                         <main id="main" role="main" itemprop="mainContentOfPage" >
               <div class="section clearfix">
-                <h2>Cinema</h2><div><h3><a href="/current-screenings">COMING SOON - ChickLit</a></h3></div>
+                <h2>Cinema</h2><!-- <div><h3><a href="/current-screenings">COMING SOON</a></h3></div> --!>
                 <?php $args = array( 'post_type' => 'capriol-film', 'production-types' => 'cinema' ); ?>
-                <?php $loop = new WP_Query( $args ); ?>
+                <?php $loop = new WP_Query($args); ?>
                 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-                              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+                              <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
                     <section class="entry-content" >
                       <a href ="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('cap-thumb'); ?></a>
                                         <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -23,10 +23,10 @@
               <div class="section clearfix">
                 <h2>Television</h2>
                 <?php $args = array( 'post_type' => 'capriol-film', 'production-types' => 'television' ); ?>
-                <?php $loop = new WP_Query( $args ); ?>
+                <?php $loop = new WP_Query($args); ?>
                 <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-                              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+                              <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
                   <section class="entry-content" >
                     <a href ="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('cap-thumb'); ?></a>
                                       <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
